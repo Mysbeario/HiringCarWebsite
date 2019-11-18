2e5456006ded10f2ac776c0097aa577cf1cd7c95
Phan Dung Tri <phandungtri4work@gmail.com>
2019-11-18 22:26:19 +0700

    Create Car Type Repository

---
+++ Infrastructure/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Infrastructure.Interfaces {
+    public interface IGenericRepository<T> where T : BaseEntity {
+        Task<IEnumerable<T>> GetAll ();
+        Task<IEnumerable<T>> GetPaginated (int page, int size);
+        Task<T> GetById (string id);
+        Task Create (T entity);
+        Task Update (string id, T entity);
+        Task Delete (string id);
+    }
+}
---
+++ Infrastructure/Repositories/CarTypeRepository.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories {
+    public class CarTypeRepository : IGenericRepository<CarType> {
+        private ApplicationContext _context;
+
+        public CarTypeRepository (ApplicationContext context) {
+            this._context = context;
+        }
+
+        public async Task<IEnumerable<CarType>> GetAll () => await _context.CarType.ToListAsync ();
+
+        public async Task<IEnumerable<CarType>> GetPaginated (int page, int size) {
+            return null;
+        }
+
+        public async Task<CarType> GetById (string id) {
+            return null;
+        }
+
+        public async Task Create (CarType entity) {
+
+        }
+        public async Task Update (string id, CarType entity) {
+
+        }
+        public async Task Delete (string id) {
+
+        }
+    }
+}
