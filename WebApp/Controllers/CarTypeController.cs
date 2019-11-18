9db251a4457fa77b5c786294cf22a4a622726562
Phan Dung Tri <phandungtri4work@gmail.com>
2019-11-18 22:26:28 +0700

    Create Car Type Controller

---
+++ WebApp/Controllers/CarTypeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers {
+    [ApiController]
+    [Route ("[controller]")]
+    public class CarTypeController : ControllerBase {
+        private CarTypeRepository carTypeRepository;
+
+        public CarTypeController () {
+            this.carTypeRepository = new CarTypeRepository (new ApplicationContext ());
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CarType>> Get () {
+            return await carTypeRepository.GetAll ();
+        }
+    }
+}
