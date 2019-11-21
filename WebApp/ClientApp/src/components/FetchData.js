dc68f75f5b6140b50f5aeda44bdbccfcbe47efc5
Phan Dung Tri <phandungtri4work@gmail.com>
2019-11-21 18:06:24 +0700

    Sketch add new car type form

--- WebApp/ClientApp/src/components/FetchData.js
+++ WebApp/ClientApp/src/components/FetchData.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Table from "./Table";
 import axios from "axios";
 import TableColumn from '../types/TableColumn';
+import Modal from './Modal/Modal';
 
 const headers = [
   new TableColumn("id", "ID"),
@@ -12,6 +13,7 @@ const headers = [
 
 const FetchData = () => {
   const [carTypes, setData] = useState([]);
+  const [isAddCarTypeFormOpened, openAddCarTypeForm] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -23,7 +25,28 @@ const FetchData = () => {
   return (
     <div>
       <Table headers={headers} data={carTypes} />
-      <button>Add new Car Type</button>
+      <button onClick={() => openAddCarTypeForm(true)}>Add new Car Type</button>
+      {isAddCarTypeFormOpened &&
+        <Modal onClose={() => openAddCarTypeForm(false)}>
+          <form>
+            <div>
+              <label>ID</label>
+              <input type="text"></input>
+            </div>
+            <div>
+              <label>Name</label>
+              <input type="text"></input>
+            </div>
+            <div>
+              <label>Number of Seat</label>
+              <input type="text"></input>
+            </div>
+            <div>
+              <label>Cost per Day (VND)</label>
+              <input type="text"></input>
+            </div>
+          </form>
+        </Modal>}
     </div>
   );
 };
