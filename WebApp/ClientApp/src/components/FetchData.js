5dea3f535740dbd969aeff2e7c1e555d97b96cca
Phan Dung Tri <phandungtri4work@gmail.com>
2019-11-22 12:05:40 +0700

    Get Car Type information for editing

--- WebApp/ClientApp/src/components/FetchData.js
+++ WebApp/ClientApp/src/components/FetchData.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Table from "./Table";
 import axios from "axios";
 import TableColumn from '../types/TableColumn';
-import Modal from './Modal/Modal';
+import CarType from "../types/CarType";
+import Modal from './Modal';
 
 const headers = [
   new TableColumn("id", "ID"),
@@ -11,30 +12,38 @@ const headers = [
   new TableColumn("cost", "Cost (VND per day)")
 ];
 
-const createNewCarType = e => {
+const createNewCarType = async e => {
   e.preventDefault();
   const data = new FormData(e.target);
   
-  axios.post("/api/cartype", data);
+  await axios.post("/api/cartype", data);
 };
 
 const FetchData = () => {
-  const [carTypes, setData] = useState([]);
+  const [carTypes, setCarTypeList] = useState([]);
+  const [currentEditedCarType, setCurrentEditedCarType] = useState(new CarType());
   const [isAddCarTypeFormOpened, openAddCarTypeForm] = useState(false);
+  const [isEditCarTypeFormOpened, openEditCarTypeForm] = useState(false);
+
+  const editCarType = id => {
+    const data = carTypes.find(item => item.id === id);
+    setCurrentEditedCarType(data);
+    openEditCarTypeForm(true);
+  };
 
   useEffect(() => {
     (async () => {
       const { data } = await axios.get("/api/cartype");
-      setData(data);
+      setCarTypeList(data);
     })();
   }, []);
 
   return (
     <div>
-      <Table headers={headers} data={carTypes} />
+      <Table headers={headers} data={carTypes} hasActionColumn onEditSelected={editCarType} />
       <button onClick={() => openAddCarTypeForm(true)}>Add new Car Type</button>
       {isAddCarTypeFormOpened &&
-        <Modal onClose={() => openAddCarTypeForm(false)}>
+        <Modal title="Add new Car Type" onClose={() => openAddCarTypeForm(false)}>
           <form onSubmit={createNewCarType}>
             <div>
               <label htmlFor="Id">ID</label>
@@ -54,7 +63,31 @@ const FetchData = () => {
             </div>
             <div>
               <button type="submit">Add</button>
-              <button>Reset</button>
+              <button type="reset">Reset</button>
+            </div>
+          </form>
+        </Modal>}
+        {isEditCarTypeFormOpened && 
+        <Modal title="Edit Car Type" onClose={() => openEditCarTypeForm(false)}>
+          <form>
+            <div>
+              <input type="hidden" name="Id" value={currentEditedCarType.id} />
+            </div>
+            <div>
+              <label htmlFor="Name">Name</label>
+              <input type="text" name="Name" defaultValue={currentEditedCarType.name}/>
+            </div>
+            <div>
+              <label htmlFor="Seat">Number of Seat</label>
+              <input type="number" name="Seat" defaultValue={currentEditedCarType.seat} />
+            </div>
+            <div>
+              <label htmlFor="Cost">Cost per Day (VND)</label>
+              <input type="number" name="Cost" defaultValue={currentEditedCarType.cost} />
+            </div>
+            <div>
+              <button type="submit">Edit</button>
+              <button type="reset">Reset</button>
             </div>
           </form>
         </Modal>}
