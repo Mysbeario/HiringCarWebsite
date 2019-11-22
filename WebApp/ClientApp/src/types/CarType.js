ff4d9072865be67d5bea2d21d3939b17bb55fd0f
Phan Dung Tri <phandungtri4work@gmail.com>
2019-11-22 12:04:22 +0700

    New CarType type for client

---
+++ WebApp/ClientApp/src/types/CarType.js
@@ -0,0 +1,10 @@
+class CarType {
+	constructor(id = "", name = "", seat = 0, cost = 0) {
+		this.id = id;
+		this.name = name;
+		this.seat = seat;
+		this.cost = cost;
+	}
+}
+
+export default CarType;
