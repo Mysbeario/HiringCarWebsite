941ee1d2f714769d092175affe66a811a3bd4192
Phan Dung Tri <phandungtri4work@gmail.com>
2019-11-24 15:55:29 +0700

    Seeding new data

--- Infrastructure/Data/ModelBuilderExtensions.cs
+++ Infrastructure/Data/ModelBuilderExtensions.cs
@@ -34,6 +34,132 @@ namespace Infrastructure.Data {
                         Name = "Kia Cerato",
                         Seat = 5,
                         Cost = 700000M
+                },
+                new CarType {
+                    Id = "6",
+                        Name = "Kia Sorento",
+                        Seat = 7,
+                        Cost = 1100000M
+                },
+                new CarType {
+                    Id = "7",
+                        Name = "Rondo",
+                        Seat = 7,
+                        Cost = 800000M
+                },
+                new CarType {
+                    Id = "8",
+                        Name = "Sendona",
+                        Seat = 8,
+                        Cost = 1600000M
+                },
+                new CarType {
+                    Id = "9",
+                        Name = "Camry",
+                        Seat = 5,
+                        Cost = 100000M
+                },
+                new CarType {
+                    Id = "10",
+                        Name = "Fortuner",
+                        Seat = 7,
+                        Cost = 1200000M
+                },
+                new CarType {
+                    Id = "11",
+                        Name = "Innova",
+                        Seat = 7,
+                        Cost = 700000M
+                },
+                new CarType {
+                    Id = "12",
+                        Name = "Toyota Corrola Altis",
+                        Seat = 5,
+                        Cost = 900000M
+                },
+                new CarType {
+                    Id = "13",
+                        Name = "Mazda 3",
+                        Seat = 5,
+                        Cost = 800000M
+                },
+                new CarType {
+                    Id = "14",
+                        Name = "Mazda CX5",
+                        Seat = 5,
+                        Cost = 1200000M
+                },
+                new CarType {
+                    Id = "15",
+                        Name = "Mescedes CLA",
+                        Seat = 5,
+                        Cost = 2000000M
+                },
+                new CarType {
+                    Id = "16",
+                        Name = "Mescedes C200",
+                        Seat = 5,
+                        Cost = 2000000M
+                },
+                new CarType {
+                    Id = "17",
+                        Name = "Mescedes C250",
+                        Seat = 5,
+                        Cost = 2200000M
+                },
+                new CarType {
+                    Id = "18",
+                        Name = "Honda City",
+                        Seat = 5,
+                        Cost = 700000M
+                },
+                new CarType {
+                    Id = "19",
+                        Name = "Honda City TOP",
+                        Seat = 5,
+                        Cost = 800000M
+                },
+                new CarType {
+                    Id = "20",
+                        Name = "Chevrolet Blazetrailer",
+                        Seat = 7,
+                        Cost = 1200000M
+                },
+                new CarType {
+                    Id = "21",
+                        Name = "Vinfast Fadil",
+                        Seat = 5,
+                        Cost = 600000M
+                },
+                new CarType {
+                    Id = "22",
+                        Name = "Huyndai Kona",
+                        Seat = 5,
+                        Cost = 900000M
+                },
+                new CarType {
+                    Id = "23",
+                        Name = "Peugoet",
+                        Seat = 5,
+                        Cost = 1400000M
+                },
+                new CarType {
+                    Id = "24",
+                        Name = "Ford Tourneo",
+                        Seat = 7,
+                        Cost = 1500000M
+                },
+                new CarType {
+                    Id = "25",
+                        Name = "Mazda 6",
+                        Seat = 4,
+                        Cost = 1300000M
+                },
+                new CarType {
+                    Id = "26",
+                        Name = "Honda Civic",
+                        Seat = 5,
+                        Cost = 1100000M
                 }
             );
             return modelBuilder;
