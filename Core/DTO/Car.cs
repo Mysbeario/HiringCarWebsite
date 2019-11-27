948c1568e1b9671347259a9304ae0e261360cf17
Phan Dung Tri <phandungtri4work@gmail.com>
2019-11-27 22:37:27 +0700

    Add Car DTO

---
+++ Core/DTO/Car.cs
@@ -0,0 +1,12 @@
+namespace Core.DTO
+{
+    public class Car
+    {
+        public string Id { get; set; }
+        public string NumberPlate { get; set; }
+        public string Color { get; set; }
+        public bool IsWifiAvailable { get; set; }
+        public string TypeId { get; set; }
+        public string TypeName { get; set; }
+    }
+}
